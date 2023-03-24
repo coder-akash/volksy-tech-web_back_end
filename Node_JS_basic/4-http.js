5b6938eb427385a03f36ad8d06729f141f682efa
coder-akash <akashd@brightcomgroup.com>
2023-03-24 06:06:46 +0000

    task done

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const hostname = 'localhost';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-  
+
 });
 module.exports = app;
