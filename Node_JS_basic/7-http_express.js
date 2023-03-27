49fc9342a8aa635ec24baed3a23d5300a2133217
coder-akash <akashd@brightcomgroup.com>
2023-03-27 07:20:45 +0000

    task done

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const countStudents = require('./3-read_file_async.js');
+
+const db = process.argv[2];
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(db);
+    res.send(`This is the list of our students\n${students.join('\n')}`);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+app.listen(1245, function () {
+});
+
+module.exports = app;
