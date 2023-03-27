d455f1906d304bc42327d5f287aeb7b771c246b9
coder-akash <akashd@brightcomgroup.com>
2023-03-27 06:26:40 +0000

    task done

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const countStudents = require('./3-read_file_async.js');
+const db = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students}');
+    try {
+      const students = await countStudents(db);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
 });
-app.get('/students', (req, res) => {
-  res.send('This is the list of our students');
+
+app.listen(port, hostname, () => {
+
 });
-app.listen(1245, function () {
-});
+module.exports = app;
