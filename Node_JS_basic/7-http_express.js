502283cc33e3229023a98620c12f16aafe7da834
coder-akash <akashd@brightcomgroup.com>
2023-03-27 07:28:48 +0000

    task done

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -13,10 +13,10 @@ app.get('/students', async (req, res) => {
     const students = await countStudents(db);
     res.send(`This is the list of our students\n${students.join('\n')}`);
   } catch (error) {
-    res.send(error.message);
+    res.send(`This is the list of our students\n${error.message}`);
   }
 });
-app.listen(1245, function () {
+app.listen(1245, () => {
 });
 
 module.exports = app;
